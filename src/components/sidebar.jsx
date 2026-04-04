a4dc65939beb29d207dbeea944a3e52a67479bd0
maryamwessam05 <23-101015@students.eui.edu.eg>
2026-04-04 17:12:48 +0200

    initial

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./sidebar.css";
+
+const Sidebar = () => {
+    return (  
+        <>  
+        
+        
+        
+        </>
+    );
+}
+ 
+export default Sidebar;
